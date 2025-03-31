872ee853b6c1566e644ad1718be5cc09a82714ee
Pedro Venâncio dos Santos <pedrosantos121229@gmail.com>
2025-03-31 16:09:25 -0300

    feat: resolução de todas as questões da atividade 1

--- pratica1.sql
+++ pratica1.sql
@@ -5,7 +5,7 @@
     --a) Crie uma role chamada ‘programadores’, com o privilégio de CRUD em todas as
     --tabelas do banco.
     CREATE ROLE programadores
-    GRANT SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA northwind TO programadores
+    GRANT USAGE, SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA northwind TO programadores
 
     --b) Crie uma role chamada ‘gerente’. Daremos privilégios para essa role nas
     --próximas questões.
@@ -18,17 +18,23 @@
     --b) Faça login com esse novo usuário
     --c) Simule falhas de conexão
     --d) Verifique os privilégios do usuário
+    SELECT * FROM pg_roles WHERE ROLNAME = 'pedro'
 
 
 --Questão 4: Concedendo privilégios ao usuário.
     --a) Considere que o usuário que você criou na questão 3 é um usuário do tipo
     --‘programador’. Insira ele na role criada na questão 2.
+    GRANT programadores TO pedro
+
     --b) Verifique o que é possível realizar no banco
+
     --c) Simule falhas associadas a falta de privilégios por esse usuário.
 
 -- Questão 5: Removendo privilégios do usuário.
     -- a) Remova da role ‘programadores’ o privilégio de realizar ‘delete’ na tabela
     -- ‘Categories’.
+    REVOKE DELETE ON northwind.categories FROM programadores;
+
     -- b) Teste a remoção do privilégio.
 
 --Questão 6 : É possível conceder privilégios sobre Views. A associação dos comandos
@@ -39,8 +45,26 @@
 --    resultado seja idêntico à figura 1. Onde:
 --        • Total_produtos é a total de produtos comprados no pedido
 --        • Total_pedido é a soma dos valores de todos os produtos comprados
+            CREATE VIEW relatorio AS
+            SELECT
+                orders.orderid,
+                orders.customerid,
+                orders.orderdate,
+                SUM(order_details.quantity) AS Total_produtos,
+                SUM(order_details.quantity * order_details.unitprice) AS Total_pedido
+            FROM
+                northwind.orders
+            JOIN northwind.order_details ON orders.orderid = order_details.orderid
+            GROUP BY orders.orderid, orders.customerid, orders.orderdate;
+
 --    b. Conceda o privilégio de leitura sobre a View para a role ‘gerente’.
+        GRANT SELECT ON relatorio TO gerente
+
 --    c. Crie o usuário ‘gestor’ e adicione ele na role
+        CREATE USER gestor WITH PASSWORD '1234'
+        GRANT gerente TO gestor
+
 --    d. Teste os privilégios do usuário gestor no banco de dados.
+        SELECT * FROM relatorio
 
 
