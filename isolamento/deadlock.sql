6b68c9569739660f2d4c7748cec97159d9139106
Pedro Venâncio dos Santos <pedrosantos121229@gmail.com>
2025-05-27 21:45:13 -0300

    docs: breve explicaçaão de como ocorre os deadlocks

--- isolamento/deadlock.sql
+++ isolamento/deadlock.sql
@@ -2,17 +2,21 @@ SELECT * FROM northwind.categories;
 
 SELECT * FROM northwind.products WHERE productid=10; 
 
---TRANSAÇÃO 1
+--TRANSAÇÃO 1 (T1)
 START TRANSACTION
+-- Primeiro
 UPDATE northwind.categories SET description= 'concorrência ativa' WHERE categoryid = 2;
-UPDATE northwind.products SET productname = 'mudanca produto' WHERE productid=10;
+-- Terceiro
+UPDATE northwind.products SET productname = 'mudanca produto' WHERE productid=10; -- gera o bloqueio (aresta de T1 para T2)
 COMMIT
 ROLLBACK
 
 
---TRANSAÇÃO 2
+--TRANSAÇÃO 2 (T2)
 START TRANSACTION
+-- Segundo
 UPDATE northwind.products SET productname = 'deadlock teste 2' WHERE productid=10;
-UPDATE northwind.categories SET description= 'concorrência ativa teste 2' WHERE categoryid = 2;
+-- Quarto
+UPDATE northwind.categories SET description= 'concorrência ativa teste 2' WHERE categoryid = 2; -- gera o deadlock (aresta de T2 para T1 formando o cilclo)
 COMMIT
 ROLLBACK
